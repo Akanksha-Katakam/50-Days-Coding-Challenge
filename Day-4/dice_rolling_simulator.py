6418e5ee59a384b6aa9507375e9626cbf02e24c8
Akanksha-Katakam <akankshakatakam@gmail.com>
2026-08-06 19:18:12 +0530

    Add Day 4 Dice rolling Simulator

---
+++ Day-4/dice_rolling_simulator.py
@@ -0,0 +1,21 @@
+import random
+
+# Function to roll the dice
+def roll_dice():
+    return random.randint(1, 6)
+
+
+while True:
+    print("\nDice Rolling Simulator")
+
+    input("Press Enter to Roll the Dice...")
+
+    number = roll_dice()
+
+    print("You rolled:", number)
+
+    choice = input("\nRoll Again? (yes/no): ").lower()
+
+    if choice == "no":
+        print("Thank you for playing!")
+        break
